636050d408eee7c8bc7669f7e2d4f1f73f6edc80
sceiiya <meetscheidj@gmail.com>
2025-03-12 23:46:44 +0800

    init:next ui

---
+++ ui/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex items-center justify-center flex-1 px-20 text-center"></main>
+    </div>
+  );
+}
